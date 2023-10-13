32a87f0c428c22aaa76b40d7f8b8eabfd87fe4ea
Joaquin <joacodileo@gmail.com>
2023-10-12 23:56:59 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,8 @@ import NuevoPost from "./components/NuevoPost";
 import EditarPost from "./components/EditarPost";
 import { useSelector } from "react-redux";
 
-axios.defaults.baseURL = "http://localhost:3001";
+/* axios.defaults.baseURL = "http://localhost:3001"; */
+axios.defaults.baseURL = "https://uccserver.onrender.com";
 
 function App() {
   const userActual = useSelector((state) => state.userActual);
