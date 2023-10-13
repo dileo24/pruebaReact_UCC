5c0084914513dae8971f8776be82175b0446a5d2
Joaquin <joacodileo@gmail.com>
2023-10-12 23:38:59 -0300

    deploy

--- server/src/db.js
+++ server/src/db.js
@@ -20,6 +20,9 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false,
   native: false,
+  define: {
+    timestamps: false,
+  },
 });
 
 const basename = path.basename(__filename);
